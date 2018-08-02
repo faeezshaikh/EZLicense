935f4ddc23917c947e1703e4f02533c02cc80837
Faeez Shaikh <faeez.shaikh@gmail.com>
2018-08-01 19:46:40 -0500

    Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class MyApp {
 
   rootPage: any = HomePage;
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{title: string, component: any,icon: string}>;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
@@ -22,7 +22,11 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'List', component: ListPage ,icon: 'list' },
+      { title: 'Projects', component: ListPage, icon: 'list' },
+      { title: 'Video Resources', component: ListPage, icon: 'logo-youtube' },
+      { title: 'Other Resources', component: ListPage, icon: 'folder' },
+      { title: 'Contact Us', component: ListPage, icon: 'people' }
     ];
 
   }
