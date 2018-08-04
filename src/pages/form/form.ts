dd3cf5db86ad68b0c09d99130759bf274bcc752b
Faeez Shaikh <faeez.shaikh@gmail.com>
2018-08-04 16:47:22 -0500

    toast msgs

--- src/pages/form/form.ts
+++ src/pages/form/form.ts
@@ -1,5 +1,5 @@
-import { Component,ViewChild } from '@angular/core';
-import { IonicPage, NavController, MenuController, NavParams,Content,AlertController,ToastController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, MenuController, NavParams, Content, AlertController, ToastController } from 'ionic-angular';
 import { HelperProvider } from '../../providers/helper/helper';
 import { ListPage } from '../list/list';
 
@@ -19,19 +19,20 @@ export class FormPage {
   confirmAbortAlert: any;
   confirmSubmitAlert: any;
   activeMenu: string;
-  somedata:string;
-  projectTitle:string;
-  odometer:any = "0";
+  somedata: string;
+  projectTitle: string;
+  odometer: any = "0";
+  showSpinner = false;
 
   @ViewChild(Content) content: Content;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,public helper: HelperProvider,
-    public menu: MenuController,public alertCtrl:AlertController,private toastCtrl: ToastController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public helper: HelperProvider,
+    public menu: MenuController, public alertCtrl: AlertController, private toastCtrl: ToastController) {
 
     helper.getData().then(theResult => {
       this.data = theResult;
       this.questions = theResult.questions;
-    //  this.questions = this.shuffle(this.questions);   /// TODO: Make shuffling of question order user configurable
+      //  this.questions = this.shuffle(this.questions);   /// TODO: Make shuffling of question order user configurable
       this.question = this.questions[0];
       console.log("Data is this => ", this.data);
       console.log("Questions => ", theResult.questions);
@@ -39,21 +40,21 @@ export class FormPage {
 
     this.questionNumber = 1;  // wil always start at 1.
     this.projectTitle = navParams.get('title');
-    console.log("Project is ",this.projectTitle);
-    
+    console.log("Project is ", this.projectTitle);
 
-   
-    
+
+
+
   }
 
-  ngAfterViewInit(){
-  //   setTimeout(function(){
-  //     document.getElementById('odometer').innerHTML = '123344';
-  //     console.log("odo -> ");
-      
-  // }, 1000);
+  ngAfterViewInit() {
+    //   setTimeout(function(){
+    //     document.getElementById('odometer').innerHTML = '123344';
+    //     console.log("odo -> ");
+
+    // }, 1000);
   }
-  
+
 
   ///// [Shuffling of Questions ] //////////
   // shuffle(a) {
@@ -109,73 +110,80 @@ export class FormPage {
     console.log('ionViewDidLoad FormPage');
   }
 
-    //////// [ Confirm Alert ] //////
-    presentAbortConfirm() {
-      this.confirmAbortAlert = this.alertCtrl.create({
-        title: 'Confirm Abort',
-        message: 'This will end the test and take you back to the home menu. Do you want to proceed?',
-        buttons: [
-          {
-            text: 'Cancel',
-            role: 'cancel',
-            handler: () => {
-              console.log('Cancel clicked');
-            }
-          },
-          {
-            text: 'Yes',
-            handler: () => {
-              console.log('Abort clicked');
-              this.navCtrl.pop();
-              // this.navCtrl.setRoot(ListPage);
-  
-            }
+  //////// [ Confirm Alert ] //////
+  presentAbortConfirm() {
+    this.confirmAbortAlert = this.alertCtrl.create({
+      title: 'Confirm Abort',
+      message: 'This will end the test and take you back to the home menu. Do you want to proceed?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
           }
-        ]
-      });
-      this.confirmAbortAlert.present();
-    }
-  
-      presentSubmitConfirm() {
-      this.confirmSubmitAlert = this.alertCtrl.create({
-        title: 'Confirm Submit',
-        message: 'This will submit your test and show you the final score. Do you want to proceed?',
-        buttons: [
-          {
-            text: 'Cancel',
-            role: 'cancel',
-            handler: () => {
-              console.log('Cancel clicked');
-            }
-          },
-          {
-            text: 'Yes',
-            handler: () => {
-              console.log('Submit clicked');
-              // Submit the exam.
-              // this.calculateAndUpdateScore();
-              this.setMode('result');
-              // this.navCtrl.pop();
-              // this.navCtrl.setRoot(ListPage);
-            }
-          }
-        ]
-      });
-      this.confirmSubmitAlert.present();
-    }
-  
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            console.log('Abort clicked');
+            this.navCtrl.pop();
+            // this.navCtrl.setRoot(ListPage);
 
-    openHelp(){
-        this.activeMenu = 'menu2';
+          }
+        }
+      ]
+    });
+    this.confirmAbortAlert.present();
+  }
+
+  presentSubmitConfirm() {
+    this.confirmSubmitAlert = this.alertCtrl.create({
+      title: 'Confirm Submit',
+      message: 'This will submit your test and show you the final score. Do you want to proceed?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            console.log('Submit clicked');
+            // Submit the exam.
+            // this.calculateAndUpdateScore();
+            this.showSpinner = true;
+            let that = this;
+            setTimeout(function () {
+              that.showSpinner = false;
+              that.setMode('result');
+              console.log('Spinner = ',that.showSpinner);
+              
+
+            }, 3000);
+
+          }
+        }
+      ]
+    });
+    this.confirmSubmitAlert.present();
+  }
+
+
+  openHelp() {
+    this.activeMenu = 'menu2';
     console.log('Menu2');
-    
+
     this.menu.enable(false, 'menu1');
     this.menu.enable(true, 'menu2');
     this.menu.open();
 
-    this.somedata ="<h3>What is Lorem Ipsum?</h3><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. <ul><li>Coffee</li><li>Tea</li><li>Milk</li></ul>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Why do we use it? It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.</p> Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>"
-    
-    }
+    this.somedata = "<h3>What is Lorem Ipsum?</h3><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. <ul><li>Coffee</li><li>Tea</li><li>Milk</li></ul>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Why do we use it? It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.</p> Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>"
+
+  }
 
 
   setMode(mode) {
@@ -194,29 +202,29 @@ export class FormPage {
     //   }
     // });
     // console.log('Questions answer',question.answer);
-    
-    if(question.answer) answered = 'Answered';
+
+    if (question.answer) answered = 'Answered';
     return answered;
   }
-  closeResults(){
+  closeResults() {
     this.navCtrl.pop();
     this.presentToast("Thank you for taking the assessment. Your assessment has been added to this list. Cheers!");
   }
- 
-  presentToast(msg:string) {
+
+  presentToast(msg: string) {
     let toast = this.toastCtrl.create({
       message: msg,
       duration: 3000,
       position: 'middle',
       cssClass: "toastClass"
     });
-  
+
     toast.onDidDismiss(() => {
       console.log('Dismissed toast');
     });
-  
+
     toast.present();
   }
-  
+
 
 }
