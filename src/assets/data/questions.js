5a4d64229ecf69ee1a1d97f33f34ff8259046d75
Faeez Shaikh <faeez.shaikh@gmail.com>
2018-08-04 12:18:17 -0500

    review page

--- src/assets/data/questions.js
+++ src/assets/data/questions.js
@@ -30,8 +30,9 @@
     },
     {
         "Id": 3,
-        "Description": "Netskope is a tool that Cyber Security at Ameren uses to evaluate external solutions. The Netskope tool has conducted tests for several commercial products in the market and assigned scores to them. To learn more, click here:", 
+        "Description": "Netskope is a tool that Cyber Security at Ameren uses to evaluate external solutions. The Netskope tool has conducted tests for several commercial products in the market and assigned scores to them. Below is an example of what a score looks like. To learn more, click here:", 
         "Name":"Is the Netskope rating of the product/solution you plan on using above 50?",
+        "Pic": "http://www.detech.com.tr/wp-content/uploads/3-3-2-netskop-bulut-guvenligi-03.png",
         "Options": [
             { "Id": 1055, "QuestionId": 1010, "Name": "Yes"},
             { "Id": 1057, "QuestionId": 1010, "Name": "No"},
@@ -42,8 +43,8 @@
     },
     {
         "Id": 4,
-        "Name": "Does the vendor own the data?", 
-        "Description":"If you plan on purchasing a product, or engaging an external partner to help with implementing your solution it is very common that the vendor will need access to Ameren data. Having access to data though is different from owning the data. Click here to learn more.",
+        "Description": "Some projects will hire a vendor who will help with implmenting a solution. Occassionaly the vendor will own or host Ameren data", 
+        "Name":"If the vendor owns data, is there a way for Ameren to download the data from the vendor?",
         "Options": [
             { "Id": 1055, "QuestionId": 1010, "Name": "Yes"},
             { "Id": 1057, "QuestionId": 1010, "Name": "No"},
@@ -54,65 +55,60 @@
     },
     {
         "Id": 5,
-        "Name": "", 
-        "Description":"",
+        "Description": "It is a very good practice to backup data to prevent from accidental loss , theft and also for disaster recovery purposes.", 
+        "Name":"Is any customer data backed up to a different location other than Ameren Data Center or Ameren AWS VPC?",
         "Options": [
-            { "Id": 1055, "QuestionId": 1010, "Name": "Promote code through the Codepipeline service.", "IsAnswer": false },
-            { "Id": 1056, "QuestionId": 1010, "Name": "Create a CI/CD Pipeline.", "IsAnswer": false },
-            { "Id": 1057, "QuestionId": 1010, "Name": "Run SQL Queries", "IsAnswer": true },
-            { "Id": 1058, "QuestionId": 1010, "Name": "Run Shell Scripts", "IsAnswer": true},
-            { "Id": 1059, "QuestionId": 1010, "Name": "Copy files", "IsAnswer": true}]
+            { "Id": 1055, "QuestionId": 1010, "Name": "Yes"},
+            { "Id": 1057, "QuestionId": 1010, "Name": "No"},
+            { "Id": 1058, "QuestionId": 1010, "Name": "Not applicable" }]
         
     },
     {
         "Id": 6,
-        "Name": "", 
-        "Description":"",
+        "Description": "From a regulatory standpoint it is very important that Ameren stores all data inside the US. Click here to learn more:", 
+        "Name":"Is the data and its backup always stored inside the US?",
         "Options": [
-              { "Id": 1055, "QuestionId": 1010, "Name": "/16", "IsAnswer": false },
-            { "Id": 1056, "QuestionId": 1010, "Name": "/24", "IsAnswer": false },
-            { "Id": 1057, "QuestionId": 1010, "Name": "/28", "IsAnswer": true },
-            { "Id": 1058, "QuestionId": 1010, "Name": "/29", "IsAnswer": false }],
+            { "Id": 1055, "QuestionId": 1010, "Name": "Yes"},
+            { "Id": 1057, "QuestionId": 1010, "Name": "No"},
+            { "Id": 1058, "QuestionId": 1010, "Name": "Not applicable" }],
         "Explanation": "Ans: /28. See link for further information",
         "Ref":"http://docs.aws.amazon.com/AmazonVPC/latest/UserGuide/VPC_Subnets.html"   
         
     },
     {
         "Id": 7,
-        "Name": "", 
-        "Description":"", 
+        "Name": "Is Ameren data encrypted at rest?", 
+        "Description":"Encryption of data is important. If bad actors get hold of Ameren data storage (databases), they will be unable to make sense of it if its encrypted using a strong encryption algorithm like SHA 256. Click here to learn more:", 
         "Options": [
-            { "Id": 1055, "QuestionId": 1010, "Name": "DynamoDBDataExists", "IsAnswer": true },
-            { "Id": 1056, "QuestionId": 1010, "Name": "ShellCommandPrecondition", "IsAnswer": true },
-            { "Id": 1057, "QuestionId": 1010, "Name": "S3KeyDoesNotExist", "IsAnswer": false },
-            { "Id": 1058, "QuestionId": 1010, "Name": "S3PrefixExists", "IsAnswer": true }],
+            { "Id": 1055, "QuestionId": 1010, "Name": "Yes"},
+            { "Id": 1057, "QuestionId": 1010, "Name": "No"},
+            { "Id": 1058, "QuestionId": 1010, "Name": "Not applicable" }],
         "Explanation": "AWS Data Pipeline provides built-in support for the following preconditions: DynamoDBDataExists,DynamoDBTableExists,S3KeyExists,S3PrefixExists,ShellCommandPrecondition",
         "Ref":"https://aws.amazon.com/datapipeline/faqs/"   
         
     },
     {
         "Id": 8,
-        "Name": "", 
-        "Description":"", "Name": "A company is using Volume Gateway to migrate on-prem data to AWS. However when the admin checks the console, he cannot see the volume data in S3. What could be the right explanation for this?", 
+        "Name": "Is Ameren data encrypted in transit?", 
+        "Description":"It is important that when you are transferring data across the internet or across networks, you encrypt data. This will prevent from 'Man in the middle attacks' - a term used to describe programs or bad actors trying to hack into the system by listening to the traffic on the wire. Ckick here to learn more:", 
         
         "Options": [
-            { "Id": 1055, "QuestionId": 1010, "Name": "Volume Gateway requires AWS Direct Connect for backing up data to AWS. The company has to use AWS Direct Connect for this to work", "IsAnswer": false },
-            { "Id": 1056, "QuestionId": 1010, "Name": "Volume Gateway needs to have a target bucket defined in S3. The admin should verify if the bucket was correctly configured. ", "IsAnswer": false},
-            { "Id": 1057, "QuestionId": 1010, "Name": "Your volumes are stored in Amazon S3 and accessible through AWS Storage Gateway. You cannot directly access them by using Amazon S3 API actions. ", "IsAnswer": true },
-            { "Id": 1058, "QuestionId": 1010, "Name": "The compnay should be using Tape Gateway to migrate the on-prem data to AWS", "IsAnswer": false }],
-        "Explanation": "Your volumes are stored in Amazon S3 and accessible through AWS Storage Gateway. You cannot directly access them by using Amazon S3 API actions. You can take point-in-time snapshots of gateway volumes that are made available in the form of Amazon EBS snapshots. Use the file interface to work with your data natively in S3.",
+            { "Id": 1055, "QuestionId": 1010, "Name": "Yes"},
+            { "Id": 1057, "QuestionId": 1010, "Name": "No"},
+            { "Id": 1058, "QuestionId": 1010, "Name": "Not applicable" }],
+         "Explanation": "Your volumes are stored in Amazon S3 and accessible through AWS Storage Gateway. You cannot directly access them by using Amazon S3 API actions. You can take point-in-time snapshots of gateway volumes that are made available in the form of Amazon EBS snapshots. Use the file interface to work with your data natively in S3.",
         "Ref":"https://aws.amazon.com/storagegateway/faqs/"  
     },
     {
         "Id": 9,
-        "Name": "", 
-        "Description":"",  "Pic": "assets/img/ques1.png",
+        "Name": "Has the vendor's data ever been breached?", 
+        "Description":"Occasionaly breaches happen. Remember the 2015 Anthem data breach or the many Yahoo security breaches? As such it is important that Ameren is aware of any historical breaches that may have occured at a vendor it plans on (or currently is) engaging",  
+        
         "Options": [
-            { "Id": 1055, "QuestionId": 1010, "Name": "Create a bastion and NAT instance in subnet-258bc44d, and add a route from rtb-238bc44b to the NAT instance.", "IsAnswer": true },
-            { "Id": 1056, "QuestionId": 1010, "Name": "Add a route from rtb-238bc44b to igw-2d8bc445 and add a bastion and NAT instance within subnet-248bc44c.", "IsAnswer": false },
-            { "Id": 1057, "QuestionId": 1010, "Name": "Create a bastion and NAT instance in subnet-248bc44c, and add a route from rtb-238bc44b to subnet-258bc44d.", "IsAnswer": false },
-            { "Id": 1058, "QuestionId": 1010, "Name": "Create a bastion and NAT instance in subnet-258bc44d, add a route from rtb-238bc44b to Igw-2d8bc445, and a new NACL that allows access between subnet-258bc44d and subnet- 248bc44c.", "IsAnswer": false }],
-        "Explanation":"Create NAT instance in public subnet which is web server subnet (suDnet-258Dc44d) and add route (rtD-238Dc44D) from private subnet (database subnet-9189c6f9) to the public NAT one to retrieve the updates."
+            { "Id": 1055, "QuestionId": 1010, "Name": "Yes"},
+            { "Id": 1057, "QuestionId": 1010, "Name": "No"},
+            { "Id": 1058, "QuestionId": 1010, "Name": "Not applicable" }],
+       "Explanation":"Create NAT instance in public subnet which is web server subnet (suDnet-258Dc44d) and add route (rtD-238Dc44D) from private subnet (database subnet-9189c6f9) to the public NAT one to retrieve the updates."
 
     }
      ,
