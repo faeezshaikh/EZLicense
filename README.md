53265dcf2879e1d81e7f622967887019cc0fb3ce
Faeez Shaikh <faeez.shaikh@gmail.com>
2018-08-07 18:23:02 -0500

    resources

--- README.md
+++ README.md
@@ -7,12 +7,14 @@ aws s3 cp --recursive ./www s3://turboarb/www
 8. Update help content ** main!
 4. Calculate score.
 5. Calculate image for score.
+
 6. Contact us page
-7. Disable create button in form
+7. Resources
 
 
 
-6. Bug with odometer css changing on clicks in sidemenu
+
+
 
 
 ------
@@ -22,7 +24,9 @@ aws s3 cp --recursive ./www s3://turboarb/www
     3. pic for netskope
     4. other pics download
 8. Open help links in new tab
+7. Disable create button in form
 5. Bug wth sidemenu
+6. Bug with odometer css changing on clicks in sidemenu
 3. Dynamically update help depending on question
 7. Ameren logo
 1. Add lastupdated to proj details
--- src/pages/resources/resources.ts
+++ src/pages/resources/resources.ts
@@ -8,14 +8,14 @@ import {  NavController, NavParams } from 'ionic-angular';
 })
 export class ResourcesPage {
 
-  resources: Array<{ no: number, icon:string,title: string, note: string,link:string}>;
+  resources: Array<{ no: number, icon:string,symbol:string,title: string, note: string,link:string}>;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.resources = [{no:1,icon:"assets/imgs/pdf-icon.ico",title:"Overview of Security Processes",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/Security/AWS_Security_Best_Practices.pdf"},
-    {no:2,icon:"assets/imgs/pdf-icon.ico",title:"Storage Options in the Cloud",note:"sdfsdfsd",link:"https://d0.awsstatic.com/whitepapers/Storage/AWS%20Storage%20Services%20Whitepaper-v9.pdf"},
-    {no:3,icon:"assets/imgs/html.png",title:"FERC Compliance Program",note:"sdfsdfsd",link:"http://scholar/NERCCompliance/Pages/FERC.aspx"},
-    {no:4,icon:"assets/imgs/html.png",title:"NERC Compliance Program",note:"sdfsdfsd",link:"http://scholar/NERCCompliance/Pages/Homepage.aspx"},
-    {no:5,icon:"assets/imgs/pdf-icon.ico",title:"Data and Information Encryption Policy",note:"sdfsdfsd",link:"http://scholar/ITPolicies/Documents/Data_Classification_Encryption/Data%20and%20Information%20Encryption%20Policy.pdf#search=Data%20and%20Information%20Encryption%20Policy"},
-    {no:6,icon:"assets/imgs/pdf-icon.ico",title:"Data and Information Classification Policy FAQs",note:"sdfsdfsd",link:"http://scholar/ITPolicies/Documents/Data_Classification_Encryption/Data%20and%20Information%20Classification%20Policy%20FAQs.pdf#search=Data%20and%20Information%20Encryption%20Policy"}];
+    this.resources = [
+    {no:1,icon:"assets/imgs/html.png",symbol:"paper-plane",title:"Ameren Architecture Patterns",note:"sdfsdfsd",link:"http://sharepoint/sites/techplan/governance/Patterns%20Documents/Forms/Architecture%20Patterns.aspx"},
+    {no:3,icon:"assets/imgs/html.png",symbol:"paper-plane",title:"FERC Compliance Program",note:"sdfsdfsd",link:"http://scholar/NERCCompliance/Pages/FERC.aspx"},
+    {no:4,icon:"assets/imgs/html.png",symbol:"paper-plane",title:"NERC Compliance Program",note:"sdfsdfsd",link:"http://scholar/NERCCompliance/Pages/Homepage.aspx"},
+    {no:5,icon:"assets/imgs/pdf-icon.ico",symbol:"md-download",title:"Ameren Data and Information Encryption Policy",note:"sdfsdfsd",link:"http://scholar/ITPolicies/Documents/Data_Classification_Encryption/Data%20and%20Information%20Encryption%20Policy.pdf#search=Data%20and%20Information%20Encryption%20Policy"},
+    {no:6,icon:"assets/imgs/pdf-icon.ico",symbol:"md-download",title:"Ameren Data and Information Classification Policy FAQs",note:"sdfsdfsd",link:"http://scholar/ITPolicies/Documents/Data_Classification_Encryption/Data%20and%20Information%20Classification%20Policy%20FAQs.pdf#search=Data%20and%20Information%20Encryption%20Policy"}];
   }
 
 
