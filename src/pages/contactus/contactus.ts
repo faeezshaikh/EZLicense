0b32fee78dc197eae3c73e7eddb46322bec2caf3
Faeez Shaikh <faeez.shaikh@gmail.com>
2018-08-07 19:59:57 -0500

    resources

--- src/pages/contactus/contactus.ts
+++ src/pages/contactus/contactus.ts
@@ -13,9 +13,10 @@ arb:any;
 
     this.arb = [
       { name: 'Joe Solari', title:'Sr. IT Director', pic:'http://uai.socious.com/higherlogic/s/media/lcfbkctq.gif',phone:'314-814-6195',email:'fshaikh@ameren.com'},
-      { name: 'Paul Straughn', title:'Director EA', pic:'https://media.licdn.com/dms/image/C5603AQGGrPvY29YajA/profile-displayphoto-shrink_200_200/0?e=1537401600&v=beta&t=e4G-PJ6ZYYNY0_oyrz9MG9G-FnRYgi18zS7AwFWoh-o',phone:'314-814-6195',email:'fshaikh@ameren.com'},
+      { name: 'Paul Straughn', title:'Director EAII', pic:'https://media.licdn.com/dms/image/C5603AQGGrPvY29YajA/profile-displayphoto-shrink_200_200/0?e=1537401600&v=beta&t=e4G-PJ6ZYYNY0_oyrz9MG9G-FnRYgi18zS7AwFWoh-o',phone:'314-814-6195',email:'fshaikh@ameren.com'},
       { name: 'Randy Allen', title:'Supervisor EA', pic:'assets/imgs/randy.jpg',phone:'314-814-6195',email:'fshaikh@ameren.com'},
       { name: 'Robin Walsh', title:'Sr. Tech Arch', pic:'https://res-1.cloudinary.com/crunchbase-production/image/upload/c_thumb,h_256,w_256,f_auto,g_faces,z_0.7,q_auto:eco/bxsblpxnluybdfdl89dn',phone:'314-814-6195',email:'fshaikh@ameren.com'},
+      { name: 'James Harris Jr.', title:'Mr. Arb', pic:'https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/11695505_869386263136883_420367730057423456_n.jpg?_nc_cat=0&oh=7fcf6304160307d02495cc44e7f931a6&oe=5BC62671',phone:'314-814-6195',email:'fshaikh@ameren.com'},
       { name: 'Faeez Shaikh', title:'Sr. Tech Arch', pic:'https://avatars3.githubusercontent.com/u/3466845?s=460&v=4',phone:'314-814-6195',email:'fshaikh@ameren.com'}
     ];
   }
