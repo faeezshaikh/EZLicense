c07ad7cee73ed8813433b5ce200f4f89ba228d77
Faeez Shaikh <faeez.shaikh@gmail.com>
2018-10-13 10:58:00 -0500

    added requester details

--- src/pages/request-details/request-details.ts
+++ src/pages/request-details/request-details.ts
@@ -10,14 +10,22 @@ import { HelperProvider } from '../../providers/helper/helper';
 export class RequestDetailsPage {
 
   request:any;
+  sliderColor:string;
   isPlatformMobile:boolean = false;
   constructor(public navCtrl: NavController, public navParams: NavParams,public viewCtrl:ViewController,private helper:HelperProvider) {
     if(this.helper.getPlatform()) this.isPlatformMobile = true;
     this.request = navParams.get('item');
+    this.updateSliderColor();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RequestDetailsPage');
+  }
+
+  updateSliderColor(){
+    if (this.request.score > 50 && this.request.score < 85) this.sliderColor = '#FFA500';
+    if (this.request.score <= 50) this.sliderColor = '#f53d3d';
+    if (this.request.score >= 85) this.sliderColor = '#32db64';
   }
   dismiss() {
 
